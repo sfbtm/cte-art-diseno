96f7ea3a67530ac75c71f466ebc121456074975e
6fbtm <dilansanv033@gmail.com>
2025-08-29 20:00:22 -0500

    Nombres de archivos arreglados

--- script/insertar.js
+++ script/insertar.js
@@ -13,6 +13,7 @@ const renderizarCursos = cursos => {
         // Crear elementos para cada curso
         const card = document.createElement("div");
         const img = document.createElement("div");
+        const contenedorTexto = document.createElement("div");
         const titulo = document.createElement("p");
         const precio = document.createElement("p");
         const boton = document.createElement("button");
@@ -20,18 +21,23 @@ const renderizarCursos = cursos => {
         // Dar clases a los elementos
         card.classList.add("galeria__contenedor__card");
         img.classList.add("galeria__contenedor__card__img");
+        contenedorTexto.classList.add("galeria__contenedor__card__texto")
         titulo.classList.add("galeria__contenedor__card__titulo");
         precio.classList.add("galeria__contenedor__card__precio");
         boton.classList.add("galeria__contenedor__card__boton");
 
         //Llenar la informacion de cada objeto
-        titulo.innerHTML = curso.titulo
+        titulo.innerHTML = curso.titulo;
+        precio.innerHTML = `$${curso.precio}`;
+        boton.innerHTML = "Comprar";
+        img.style.backgroundImage = `url(/../src/curso-${curso.id}/1.jpeg)`
 
         //Emparentar objetos
         contenedor.appendChild(card);
         card.appendChild(img);
-        card.appendChild(titulo);
-        card.appendChild(precio);
+        card.appendChild(contenedorTexto);
+        contenedorTexto.appendChild(titulo);
+        contenedorTexto.appendChild(precio);
         card.appendChild(boton);
     })
     
